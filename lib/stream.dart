04d5ac116448a92b757e5cedc83dd008d797c185
yuma rakha samodra sikayo <sikayoyuma@gmail.com>
2024-12-04 13:40:48 +0700

    Praktikum 2

--- lib/stream.dart
+++ lib/stream.dart
@@ -1,12 +1,13 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
-class ColorStream{
+class ColorStream {
   final List<Color> colors = [
     Colors.blueGrey,
     Colors.amber,
     Colors.deepPurple,
     Colors.lightBlue,
-    Colors.teal, 
+    Colors.teal,
     Colors.blue,
     Colors.red,
     Colors.lightBlue,
@@ -15,13 +16,27 @@ class ColorStream{
   ];
 
   Stream<Color> getColors() async* {
-    yield* Stream.periodic(
-        const Duration(seconds: 1), (int t){
-            int index = t % colors.length;
-            return colors[index];
-        }
-    );
+    yield* Stream.periodic(const Duration(seconds: 1), (int t) {
+      int index = t % colors.length;
+      return colors[index];
+    });
 
     // fungsi dari yield adalah untuk menghasilkan nilai dari fungsi generator secara berurutan
+  }
+}
+
+class NumberStream {
+  final StreamController<int> controller = StreamController<int>();
+
+  void addNumberToSink(int newNumber) {
+    controller.sink.add(newNumber);
+  }
+
+  close() {
+    controller.close();
+  }
+
+  addError(){
+    controller.sink.addError('error');
   }
 }
