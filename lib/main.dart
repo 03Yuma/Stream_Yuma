a3c20e3fe4aa26964a765623d4fa78c49cf98692
yuma rakha samodra sikayo <sikayoyuma@gmail.com>
2024-12-04 13:04:53 +0700

    soal 1 - 5

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,5 @@
 import 'package:flutter/material.dart';
-
-class ColorStream{
-  final List<Color> colors = [
-    Colors.blueGrey,
-    Colors.amber,
-    Colors.deepPurple,
-    Colors.lightBlue,
-    Colors.teal 
-  ];
-}
+import 'stream.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,118 +8,53 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Nomer 1
-      title: 'Yuma Rakha Samodra Sikayo',
+      title: 'Yuma',
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
+        primarySwatch: Colors.deepPurple, // Tema warna utama aplikasi.
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const StreamHomePage(),
     );
   }
 }
 
-class StreamHomePage extends StatefulWidget{
+class StreamHomePage extends StatefulWidget {
   const StreamHomePage({super.key});
-  
-  @override 
-  State<StreamHomePage> createState()=>_StreamHomePageState();
+
+  @override
+  State<StreamHomePage> createState() => _StreamHomePageState();
 }
 
 class _StreamHomePageState extends State<StreamHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
+  Color bgColor = Colors.blueGrey;
+  late ColorStream colorStream;
 
-Stream<Color> getColors() async* {
-  final colorStream = ColorStream();  
-  yield* Stream.periodic(
-    const Duration(seconds: 1), (int t) {
-      int index = t % colorStream.colors.length;  
-      return colorStream.colors[index];
-    },
-  );
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  void changeColor() async {
+    colorStream.getColors().listen((eventColor) {
+      setState(() {
+        bgColor = eventColor;
+      });
     });
   }
 
   @override
+  void initState(){
+    super.initState;
+    colorStream = ColorStream();
+    changeColor();
+  }
+  
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Stream'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: Container(
+        decoration: BoxDecoration(color: bgColor),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ lib/stream.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ColorStream{
+  final List<Color> colors = [
+    Colors.blueGrey,
+    Colors.amber,
+    Colors.deepPurple,
+    Colors.lightBlue,
+    Colors.teal, 
+    Colors.blue,
+    Colors.red,
+    Colors.lightBlue,
+    Colors.blueAccent,
+    Colors.black12
+  ];
+
+  Stream<Color> getColors() async* {
+    yield* Stream.periodic(
+        const Duration(seconds: 1), (int t){
+            int index = t % colors.length;
+            return colors[index];
+        }
+    );
+
+    // fungsi dari yield adalah untuk menghasilkan nilai dari fungsi generator secara berurutan
+  }
+}
